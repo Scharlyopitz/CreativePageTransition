0fecbbcbaec69d8396d69719be5251d82a6233e0
Scharly <scharly.opitz@gmail.com>
2025-02-18 13:22:37 +0100

    Ajout animatePresence

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import ErrorPage from "./pages/ErrorPage";
 import Nav from "./components/Nav";
+import { AnimatePresence } from "motion/react";
 
 function App() {
+  const { pathname } = useLocation();
   return (
     <>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <AnimatePresence>
+        <Routes location={pathname} key={pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="*" element={<ErrorPage />} />
+        </Routes>
+      </AnimatePresence>
     </>
   );
 }
