e50a9f7695be1bbdcaf0f6f78372c6270bae7e11
Scharly <scharly.opitz@gmail.com>
2025-02-18 14:49:17 +0100

    Changement code pour page transition

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,20 +1,7 @@
 import { motion as m } from "motion/react";
+import { animePageContent } from "./Animations";
 
 export default function About() {
-  const animePageContent = {
-    initial: {
-      y: "102%",
-    },
-    animate: {
-      y: 0,
-      transition: {
-        duration: 2,
-      },
-    },
-    exit: {
-      y: 0,
-    },
-  };
   return (
     <m.main
       initial="initial"
---
+++ src/pages/Animations.jsx
@@ -0,0 +1,19 @@
+export const animePageContent = {
+  initial: {
+    y: "102%",
+  },
+  animate: {
+    y: 0,
+    transition: {
+      duration: 1,
+      ease: [0.65, 0, 0.35, 1],
+    },
+  },
+  exit: {
+    y: 1,
+    transition: {
+      duration: 1,
+      ease: [0.65, 0, 0.35, 1],
+    },
+  },
+};
