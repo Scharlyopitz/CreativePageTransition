d38c5a15da09d352275a14bb05b74937475e2219
Scharly <scharly.opitz@gmail.com>
2025-02-18 12:44:43 +0100

    Ajout des différentes pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import ErrorPage from "./pages/ErrorPage";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,3 @@
+export default function ErrorPage() {
+  return <main>EROR 404</main>;
+}
