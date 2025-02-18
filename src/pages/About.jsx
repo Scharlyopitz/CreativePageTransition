346fab752ce1f5b6b11539528d6d5f5b3c42dca7
Scharly <scharly.opitz@gmail.com>
2025-02-18 13:38:05 +0100

    Ajout animation pour les pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,29 @@
+import { motion as m } from "motion/react";
+
 export default function About() {
+  const animePageContent = {
+    initial: {
+      y: "102%",
+    },
+    animate: {
+      y: 0,
+      transition: {
+        duration: 2,
+      },
+    },
+    exit: {
+      y: 0,
+    },
+  };
   return (
-    <main id="about">
+    <m.main
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      variants={animePageContent}
+      id="about"
+    >
       <h1>about</h1>
-    </main>
+    </m.main>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,28 @@
+import { motion as m } from "motion/react";
+
 export default function Home() {
+  const animePageContent = {
+    initial: {
+      y: "102%",
+    },
+    animate: {
+      y: 0,
+      transition: {
+        duration: 2,
+      },
+    },
+    exit: {
+      y: 0,
+    },
+  };
   return (
-    <main>
+    <m.main
+      initial="initial"
+      animate="animate"
+      exit="exit"
+      variants={animePageContent}
+    >
       <h1>Home</h1>
-    </main>
+    </m.main>
   );
 }
